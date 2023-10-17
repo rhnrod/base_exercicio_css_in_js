9dbf94979e5331e8862e595db183c136918661e4
Rhuan Emanuel <rhnrod@gmail.com>
2023-10-16 22:33:26 -0300

    fix components

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -1,13 +1,18 @@
-.form {
+import styled from 'styled-components'
+
+export const Form = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
   background-color: var(--cor-secundaria);
   padding: 32px;
   border-radius: 12px;
   margin-top: 40px;
-}
 
-.btnPesquisar {
+  @media (max-width: 768px) {
+    display: block;
+  }
+`
+export const BotaoPesquisar = styled.button`
   background-color: var(--cor-principal);
   border: 1px solid var(--cor-principal);
   height: 40px;
@@ -16,9 +21,20 @@
   color: var(--cor-secundaria);
   margin-left: 8px;
   cursor: pointer;
-}
 
-.campo {
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-left: 0;
+    margin-top: 8px;
+  }
+`
+export const Campo = styled.input`
   padding: 0 16px;
   outline-color: var(--cor-principal);
-}
+
+  @media (max-width: 768px) {
+    text-align: center;
+    padding: 16px;
+    width: 100%;
+  }
+`
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,13 +1,11 @@
-import styles from './Hero.module.css'
+import { Hero as HeroEstilo, Title } from './styles'
 
 const Hero = () => (
-  <form className={styles.form}>
+  <HeroEstilo>
     <div className="container">
-      <h2 className={styles.heroTitle}>
-        As melhores vagas para tecnologia, design e artes visuais.
-      </h2>
+      <Title>As melhores vagas para tecnologia, design e artes visuais.</Title>
     </div>
-  </form>
+  </HeroEstilo>
 )
 
 export default Hero
